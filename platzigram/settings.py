063b8d2c8a068b7dd68d9c1ba9dc35a242650864
Nelson Acosta <nelson.acosta.web@gmail.com>
2020-06-23 10:43:12 -0400

    añadiendo cambio

--- platzigram/settings.py
+++ platzigram/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'platzigramdb',
         'USER': 'platzigram',
-        'PASSWORD': 'tyckyBoom01.',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
